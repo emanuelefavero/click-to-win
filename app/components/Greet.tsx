066b6886218ea299cadbbed88af73b2772739dad
emanuelefavero <emafaux89@gmail.com>
2024-10-02 14:28:45 +0200

    remove HeaderSignInButton to prevent content shifting

--- app/components/Greet.tsx
+++ app/components/Greet.tsx
@@ -1,22 +1,11 @@
 import { currentUser } from '@clerk/nextjs/server'
-import { SignInButton } from '@clerk/nextjs'
 
 export default async function Greet() {
   const user = await currentUser()
 
   return (
     <h1 className='ml-3 font-semibold select-none text-base'>
-      {user ? (
-        `Hello, ${user.firstName}`
-      ) : (
-        <>
-          <SignInButton>
-            <button className='bg-transparent border-none px-0 py-0 hover:bg-transparent'>
-              <span className='mr-1'>👆</span> Click to Win
-            </button>
-          </SignInButton>
-        </>
-      )}
+      {user ? `Hello, ${user.firstName}` : '👆 Click to Win'}
     </h1>
   )
 }
