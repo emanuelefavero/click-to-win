21f03c6a9258edc7379e63f403090be0ea2fe8e1
emanuelefavero <emafaux89@gmail.com>
2024-10-02 14:53:00 +0200

    add - only update count in json file if new count is greater

--- app/actions.ts
+++ app/actions.ts
@@ -1,9 +1,20 @@
 'use server'
 
-import { revalidatePath } from 'next/cache'
 import fs from 'fs'
+import path from 'path'
+import { revalidatePath } from 'next/cache'
 
-export async function incrementCount(count: number) {
-  fs.writeFileSync('./data/count.json', JSON.stringify({ count }))
+const filePath = path.join(process.cwd(), 'data/count.json')
+
+export async function incrementCount(newCount: number) {
+  // Read the current count from the JSON file
+  const data = fs.readFileSync(filePath, 'utf-8')
+  const count = JSON.parse(data).count
+
+  // Only update the JSON file if the new count is greater
+  if (newCount > count) {
+    fs.writeFileSync(filePath, JSON.stringify({ count: newCount }), 'utf-8')
+  }
+
   revalidatePath('/')
 }
