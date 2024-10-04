a71de1cdc0a0b448f2e54a59da9ba7d4e7318e40
emanuelefavero <emafaux89@gmail.com>
2024-10-04 16:38:25 +0200

    add svg spinner icon to disabled submit button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -11,7 +11,7 @@ export default function Button({ children, type, className, onClick }: Props) {
   return (
     <button
       type={type}
-      className={`text-2xl mt-2 w-full py-2 select-none ${className}`}
+      className={`text-2xl mt-2 w-full min-w-20 min-h-12 py-2 select-none ${className}`}
       onClick={onClick}
     >
       {children}
