ef830f4371c70cca1af3b544b6b14db882c03b5f
emanuelefavero <emafaux89@gmail.com>
2024-10-07 18:26:03 +0200

    add styling in WinModal

--- components/WinModal.tsx
+++ components/WinModal.tsx
@@ -8,24 +8,30 @@ export default async function Component() {
 
   return (
     <div className='fixed top-0 left-0 w-full h-full bg-white/50 dark:bg-black/50 backdrop-blur-sm flex justify-center items-center z-50 p-4 overflow-auto'>
-      <div className='bg-[#f0f9ff] dark:bg-[#030816] border border-slate-400 dark:border-slate-600 rounded-lg shadow-md shadow-slate-200 dark:shadow-black px-5 py-6 text-center relative max-w-sm animate-slideFromTop'>
-        <h2 className='text-lg'>
-          Congratulations{user && ` ${user.firstName}`}! 🎉
+      <div className='bg-[#f0f9ff] dark:bg-[#030816] border border-slate-400 dark:border-slate-600 rounded-lg shadow-md shadow-slate-200 dark:shadow-black px-5 py-6 text-center relative max-w-md animate-slideFromTop'>
+        <h2 className='text-2xl mb-5'>
+          Congratulations
+          {user && `, ${user.firstName}`}! 🎉
         </h2>
 
-        <p>You are the first to reach 100! 🏆</p>
+        <p className='mb-3 text-slate-400'>
+          You are the first to reach{' '}
+          <span className='font-bold text-white'>100</span>! 🏆
+        </p>
 
-        <p>
+        <p className='mb-3 text-slate-400'>
           Please check your{' '}
           {user && (
-            <span className='font-bold'>
+            <span className='font-bold text-white'>
               {user.primaryEmailAddress?.emailAddress}
             </span>
           )}{' '}
           email for more details on how to claim your prize.
         </p>
 
-        <Link href='/'>Close</Link>
+        <Link href='/' className='text-xl'>
+          Close
+        </Link>
       </div>
     </div>
   )
