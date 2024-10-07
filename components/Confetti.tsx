e0cc9711af4014699f531d661c3dd82e9d42e167
emanuelefavero <emafaux89@gmail.com>
2024-10-07 19:45:42 +0200

    customize react confetti

--- components/Confetti.tsx
+++ components/Confetti.tsx
@@ -1,11 +1,27 @@
 'use client'
 
-import React from 'react'
 import Confetti from 'react-confetti'
 import useWindowSize from '@/hooks/useWindowSize'
 
 export default function Component() {
   const { width, height } = useWindowSize()
 
-  return <Confetti width={width} height={height} />
+  return (
+    <Confetti
+      width={width}
+      height={height}
+      colors={[
+        '#198441',
+        '#22b975',
+        '#0b3b1d',
+        '#5e8532',
+        '#07896d',
+        '#42e17c',
+        '#955a2a',
+      ]}
+      wind={0.02}
+      opacity={0.8}
+      numberOfPieces={150}
+    />
+  )
 }
