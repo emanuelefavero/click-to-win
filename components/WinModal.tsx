9d9f195e2eb1fb503652153633827468361c6ceb
emanuelefavero <emafaux89@gmail.com>
2024-10-07 16:26:18 +0200

    refactor Modal css to Tailwind

--- components/WinModal.tsx
+++ components/WinModal.tsx
@@ -1,11 +1,10 @@
-import './WinModal.css'
 import Link from 'next/link'
 
 export default function Component() {
   return (
-    <div className='modal-overlay'>
-      <div className='modal-content'>
-        <h2>You Won!</h2>
+    <div className='fixed top-0 left-0 w-full h-full bg-black/50 backdrop-blur-sm flex justify-center items-center z-50 p-4 overflow-auto'>
+      <div className='bg-[#030816] border border-slate-600 rounded-lg shadow-md px-5 py-4 text-center relative max-w-prose animate-slideFromTop'>
+        <h2>You Won! 🎉</h2>
 
         <Link href='/'>Close</Link>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,36 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+
+      keyframes: {
+        slideFromTop: {
+          '0%': {
+            transform: 'translateY(-100%)',
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'translateY(0)',
+            opacity: '1',
+          },
+        },
+      },
+
+      animation: {
+        slideFromTop: 'slideFromTop 0.3s ease-out',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
