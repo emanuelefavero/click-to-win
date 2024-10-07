ed9571026f9b69a209694bab71acd0aa8a0d5a9e
emanuelefavero <emafaux89@gmail.com>
2024-10-07 18:48:16 +0200

    udpate WinModal link

--- components/WinModal.tsx
+++ components/WinModal.tsx
@@ -20,7 +20,7 @@ export default async function Component() {
           ! 🏆
         </p>
 
-        <p className='mb-3 text-slate-600 dark:text-slate-400'>
+        <p className='mb-4 text-slate-600 dark:text-slate-400'>
           Please check your{' '}
           {user && (
             <span className='font-bold text-slate-600 dark:text-slate-300'>
@@ -30,7 +30,7 @@ export default async function Component() {
           email for more details on how to claim your prize.
         </p>
 
-        <Link href='/' className='text-xl'>
+        <Link href='/' className='text-2xl select-none'>
           Close
         </Link>
       </div>
