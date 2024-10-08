eba826ba2e9f2b71d3342c8c70ce43fe8bcd2745
emanuelefavero <emafaux89@gmail.com>
2024-10-08 15:08:09 +0200

    add gradient text to Title

--- components/Title.tsx
+++ components/Title.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Component() {
   return (
     <>
-      <h1 className='uppercase text-2xl mb-5 select-none text-center'>
+      <h1 className='uppercase text-2xl mb-5 select-none text-center bg-gradient-to-r from-cyan-600 via-green-500 to-teal-400 inline-block text-transparent bg-clip-text'>
         Click to Win
       </h1>
       <p className='mb-5 text-slate-600 dark:text-slate-400 select-none text-center'>
