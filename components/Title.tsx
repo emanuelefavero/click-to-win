1a651c31ac8e92191db654d8066c5eb34cdb686b
emanuelefavero <emafaux89@gmail.com>
2024-10-08 13:35:18 +0200

    fix text alignment in Title

--- components/Title.tsx
+++ components/Title.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 export default function Component() {
   return (
     <>
-      <h1 className='uppercase text-2xl mb-5 select-none'>Click to Win</h1>
-      <p className='mb-5 text-slate-600 dark:text-slate-400 select-none'>
+      <h1 className='uppercase text-2xl mb-5 select-none text-center'>
+        Click to Win
+      </h1>
+      <p className='mb-5 text-slate-600 dark:text-slate-400 select-none text-center'>
         The first to reach 100 wins a prize!
       </p>
     </>
