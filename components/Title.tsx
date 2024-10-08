cf654697db405f6deb39658a202f9b95f3f92a5a
emanuelefavero <emafaux89@gmail.com>
2024-10-08 13:33:47 +0200

    add light mode styling in Title

--- components/Title.tsx
+++ components/Title.tsx
@@ -4,7 +4,7 @@ export default function Component() {
   return (
     <>
       <h1 className='uppercase text-2xl mb-5 select-none'>Click to Win</h1>
-      <p className='mb-5 text-slate-400 select-none'>
+      <p className='mb-5 text-slate-600 dark:text-slate-400 select-none'>
         The first to reach 100 wins a prize!
       </p>
     </>
