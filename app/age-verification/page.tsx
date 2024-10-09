e2e019fb0a248f9fafaca9a7b7111522226eff68
emanuelefavero <emafaux89@gmail.com>
2024-10-09 19:50:21 +0200

    create age verification component

--- app/age-verification/page.tsx
+++ app/age-verification/page.tsx
@@ -1,34 +1,16 @@
-import { redirect } from 'next/navigation'
-import { cookies } from 'next/headers'
+import Header from '@/components/Header'
+import Main from '@/components/Main'
+import Title from '@/components/Title'
+import AgeVerification from '@/components/AgeVerification'
 
 export default function Page() {
-  async function yes() {
-    'use server'
-
-    cookies().set('isOver18', 'true')
-    redirect('/')
-  }
-
-  async function no() {
-    'use server'
-
-    cookies().set('isOver18', 'false')
-    redirect('https://google.com')
-  }
-
   return (
     <>
-      <h2>Age Verification</h2>
-
-      <p>Are you 18 or older?</p>
-
-      <form action={yes}>
-        <button type='submit'>Yes</button>
-      </form>
-
-      <form action={no}>
-        <button type='submit'>No</button>
-      </form>
+      <Header />
+      <Main>
+        <Title />
+        <AgeVerification />
+      </Main>
     </>
   )
 }
---
+++ components/AgeVerification.tsx
@@ -0,0 +1,36 @@
+import { redirect } from 'next/navigation'
+import { cookies } from 'next/headers'
+
+export default function Component() {
+  async function yes() {
+    'use server'
+
+    cookies().set('isOver18', 'true')
+    redirect('/')
+  }
+
+  async function no() {
+    'use server'
+
+    cookies().set('isOver18', 'false')
+    redirect('https://google.com')
+  }
+
+  return (
+    <>
+      <h2 className='text-3xl font-bold select-none mb-7'>
+        Are you 18 or older?
+      </h2>
+
+      <div className='flex gap-3'>
+        <form action={yes}>
+          <button type='submit'>Yes - Enter</button>
+        </form>
+
+        <form action={no}>
+          <button type='submit'>No - Exit</button>
+        </form>
+      </div>
+    </>
+  )
+}
